db575d6a58bfc41353973c6197e6e22b1f7b3fb8
ankit <ankitsinghwakefit@gmail.com>
2020-01-24 12:51:03 +0530

    completed 2 more exercise

---
+++ assignments/3. class/main.js
@@ -0,0 +1,23 @@
+class BookList{
+  constructor(completedBooks, pendingBooks){
+    this.completedBooks = completedBooks;
+    this.pendingBooks = pendingBooks;
+  }
+  nextBook {
+      book1 : "ank",
+  }
+}
+var nextBook = {
+
+}
+var currentBook = {
+
+}
+
+var lastBook = {
+
+}
+
+class Book {
+
+}
